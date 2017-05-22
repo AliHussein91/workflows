966fa6e494b57d7db08316c894ab637aab766dbf
Ali Hussein <Ali Hussein>
2017-05-22 12:13:46 +0200

    Concatenated JavaScript Files

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,12 @@ var gulp = require('gulp'),
 	coffee = require('gulp-coffee'),
 	concat  = require('gulp-concat');
 
-var coffeeSources = ['components/coffee/tagline.coffee'];
+var coffeeSources = ['components/coffee/tagline.coffee'],
+	jsSources = ['components/scripts/rclick.js',
+				'components/scripts/pixgrid.js',
+				'components/scripts/tagline.js',
+				'components/scripts/template.js'
+				];
 
 gulp.task('coffee', function(){
 	gulp.src(coffeeSources)
@@ -11,3 +16,9 @@ gulp.task('coffee', function(){
 		.on('error', gutil.log))
 	.pipe(gulp.dest('components/scripts'))
 });
+
+gulp.task('js', function(){
+	gulp.src(jsSources)
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('builds/development/js'))
+})
